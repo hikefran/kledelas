40be8d02e93c62512850a0ba00d66e4520609e19
Henrique <129874242+hikefran@users.noreply.github.com>
2023-11-29 01:43:32 -0300

    pppp

--- terraform/azure/provider.tf
+++ terraform/azure/provider.tf
@@ -1,20 +1,19 @@
 terraform {
   required_providers {
     azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">= 3.77.0"
+      source = "hashicorp/azurerm"
     }
   }
+
 #   backend "azurerm" {
-#     resource_group_name  = "nader-gs"
-#     storage_account_name = "nader3568"
-#     container_name       = "tfstate"
+#     resource_group_name  = "iac"
+#     storage_account_name = "terraformstatebvw"
+#     container_name       = "terraformstatebrunowolff"
 #     key                  = "terraform.tfstate"
 #   }
-}
+ }
 
 provider "azurerm" {
   features {}
-  skip_provider_registration = true
-  alias                      = "cloud"
+  use_msi = true
 }
